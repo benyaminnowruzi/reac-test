5ac4e8a6f8b0ac220800e40650daabdd9ee80b32
Benyamin <benyaminnowruzi@gmail.com>
2024-12-31 19:35:06 +0330

    change js file to jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {Input} from "./components"
+function App() {
+  return (
+    <div className="App">
+      <Input />
+    </div>
+  );
+}
+
+export default App;
